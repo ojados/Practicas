26a1eefd9381493548dad26c1ddc9a2d16963f66
Daniel Ojados <dojados@outlook.com>
2024-01-11 11:29:48 +0100

    Versión Inicial

---
+++ src/testgithub/TestGithub.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testgithub;
+
+/**
+ *
+ * @author danie
+ */
+public class TestGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// esto es un comentario chorra
+        System.out.println(" Hola mundo mundial");
+    }
+    
+}
